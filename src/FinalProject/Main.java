3389e8e52ff7e88524c4ecb4b8d9fd3bf57ef469
JapinanClarence <japinanclarence@gmail.com>
2022-12-22 22:00:06 +0800

    Commit

--- src/FinalProject/Main.java
+++ src/FinalProject/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -12,21 +15,21 @@ public class Main {
         do {
             //Shows the system menu
             System.out.println("""
-                           *----Codebyters Officers and Members Directory System----*
-                       
-                           System Menu
-                           *--------------------------------------*"   
-                           a. Register
-                           b. Sign Up
-                           c. View Members
-                           d. Exit
-                           """);
+                                *----Codebyters Officers and Members Directory System----*
+                            
+                                System Menu
+                                *--------------------------------------*  
+                                a. Register
+                                b. Sign Up
+                                c. View Members
+                                d. Exit
+                                """);
             System.out.print("Enter option: ");
             char operator = scan.next().charAt(0);
 
                 switch (operator) {
                     case 'a' -> {
-                        Transaction.register();
+
                     }
                     case 'b' -> System.out.println("Register Position");
                     case 'c' -> System.out.println("View Members");
@@ -37,7 +40,6 @@ public class Main {
                     default -> throw new IllegalStateException("Unexpected value: " + operator);
                 }
         } while (loop == "yes");
-
 
     }
 }
