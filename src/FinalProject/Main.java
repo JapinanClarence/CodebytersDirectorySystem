10aa2c4fec794cd4f918b4ee10edc34a27fb2e45
JapinanClarence <japinanclarence@gmail.com>
2022-12-22 21:21:22 +0800

    project initialized

---
+++ src/FinalProject/Main.java
@@ -0,0 +1,44 @@
+package FinalProject;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        String loop = "yes";
+        do {
+            //Shows the system menu
+            System.out.println("""
+                           *----Codebyters Officers and Members Directory System----*
+                       
+                           System Menu
+                           *--------------------------------------*"   
+                           a. Register
+                           b. Sign Up
+                           c. View Members
+                           d. Exit
+                           """);
+            System.out.print("Enter option: ");
+            char operator = scan.next().charAt(0);
+
+                switch (operator) {
+                    case 'a' -> {
+                        Transaction.register();
+                    }
+                    case 'b' -> System.out.println("Register Position");
+                    case 'c' -> System.out.println("View Members");
+                    case 'd' -> {
+                        loop = "no";
+                        System.out.println("Program Terminated");
+                    }
+                    default -> throw new IllegalStateException("Unexpected value: " + operator);
+                }
+        } while (loop == "yes");
+
+
+    }
+}
